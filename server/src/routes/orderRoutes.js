fdd3d3c89583d33a35b9f638fac2253645bc0d81
Shree <pandurangteke95@gmail.com>
2025-05-28 15:55:35 +0530

    added get order

--- server/src/routes/orderRoutes.js
+++ server/src/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 // routes/orderRoutes.js
 const express = require('express');
-const { createOrder,updateOrder } = require('../controller/orderController');
+const { createOrder,updateOrder,getOrders } = require('../controller/orderController');
 const authMiddleware = require('../middleware/authMiddleware'); // if using auth
 const router = express.Router();
 
 router.post('/create', authMiddleware, createOrder);
 router.post('/update', authMiddleware, updateOrder);
+router.get('/get', authMiddleware,getOrders);
 
 
 module.exports = router;
