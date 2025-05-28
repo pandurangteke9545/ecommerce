0b5701953d6cb6ad783de9e248840e740eda1d5a
Shree <pandurangteke95@gmail.com>
2025-05-28 15:54:33 +0530

    logout added

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controller/authController.js');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/logout',authController.logout)
 
 module.exports = router;
