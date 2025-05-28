9b6201f99aa572526ff6bb82c7537835bbe4bd87
Shree <pandurangteke95@gmail.com>
2025-05-28 15:56:29 +0530

    added order

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,6 +41,7 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Cart from "./pages/Cart";
+import Order from "./pages/order"
 import PaymentSuccess from "./pages/PaymentSuccess";
 import PaymentFailure from "./pages/PaymentFailure";
 
@@ -55,6 +56,7 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Order/>} />
         <Route path="/payment-success" element={<PaymentSuccess />} />
         <Route path="/payment-failure" element={<PaymentFailure />} />
       </Routes>
