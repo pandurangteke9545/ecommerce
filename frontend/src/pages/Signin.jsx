174e7a21d3b9180359cb722f8d9764ecf5bb81cf
PandurangTeke-Qicpic <pandurang.teke@qicpic.org>
2025-08-19 16:52:18 +0530

    added toast

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,80 +1,10 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import api from "../api/api";
-
-
-
-
-// const Signin = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleSignin = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await api.post(
-//         "/auth/login",
-//         { email, password },
-//         { withCredentials: true } // ✅ Ensure cookies are stored
-//       );
-//       alert(res.data.message || "Login successful");
-//       navigate("/");
-//     } catch (err) {
-//       alert(err.response?.data?.message || "Login failed");
-//     }
-//   };
-
-//   return (
-//     <div className="max-w-md mx-auto mt-10 p-6 border rounded shadow">
-//       <h2 className="text-2xl font-bold mb-4">Sign In</h2>
-//       <form onSubmit={handleSignin} className="space-y-4">
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           className="w-full border px-4 py-2 rounded"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//           required
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           className="w-full border px-4 py-2 rounded"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           required
-//         />
-//         <button
-//           type="submit"
-//           className="w-full bg-blue-600 text-white py-2 rounded"
-//         >
-//           Sign In
-//         </button>
-
-//          <p className="text-sm text-center">
-//           Don’t have an account?{" "}
-//           <span
-//             onClick={() => navigate("/signup")}
-//             className="text-blue-600 hover:underline cursor-pointer"
-//           >
-//             Sign Up
-//           </span>
-//         </p>
-
-
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Signin;
-
-
-
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/api";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const Signin = () => {
   const [email, setEmail] = useState("");
@@ -99,9 +29,11 @@ const Signin = () => {
 
       // ✅ Trigger auth state update (for Navbar or other components)
       window.dispatchEvent(new Event("authChange"));
-
-      alert(res.data.message || "Login successful");
-      navigate("/");
+       toast.success('Login Successfully !')
+       setTimeout(()=>{
+          navigate("/");
+       },1100)
+      
     } catch (err) {
       alert(err.response?.data?.message || "Login failed");
     }
@@ -143,6 +75,9 @@ const Signin = () => {
           </span>
         </p>
       </form>
+      <ToastContainer
+      autoClose='1000'
+      />
     </div>
   );
 };
